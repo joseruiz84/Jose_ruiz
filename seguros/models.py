4868f6a72074275312c8a4610b9494c45a92cb00
RUIZ José Antonio <RuizJ@sancristobal.com.ar>
2023-03-21 00:47:58 -0300

    se agrego un modelo

--- seguros/models.py
+++ seguros/models.py
@@ -1,4 +1,17 @@
+#from django.db import models
+
+#class Post(models.Model):
+#    pass
+
+#nuevo
+
 from django.db import models
 
-class post(models.Model):
-    pass
+class Post(models.Model):
+     Suma_asegurada = models.CharField(max_length=30)
+     Suma_description = models.CharField(max_length=80)
+     heading = models.CharField(max_length=15)
+    
+   
+     def __str__(self):
+        return f"{self.id} - {self.Suma_description}"
